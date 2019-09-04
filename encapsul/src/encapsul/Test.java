a69d1c3ebbfcf343bfc95758fed0f14425414d82
samarjeet <samrjeetpowalkar@gmail.com>
2019-09-04 06:41:57 -0700

    shankar

---
+++ encapsul/src/encapsul/Test.java
@@ -0,0 +1,27 @@
+package encapsul;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+for(int i=0;i<10;i++)
+{Scanner in = new Scanner(System.in);
+	setgetr  si=new setgetr();
+	si.setRollno (i);
+	System.out.println("Roll Number-");
+	System.out.println(+si.getRollno ());
+	System.out.println("Enter the name-");
+	si.setName (in.nextLine());
+	System.out.println(si.getName ());
+	
+	
+}
+		
+		
+
+		
+		
+	}
+	
+	
+}
